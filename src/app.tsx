9ad65869d1c07a13f9d5b12b8c377e50671e9152
Quentin Ventura <ventura.quentin@gmail.com>
2018-06-21 11:06:06 -0400

    Initialize project with ReactStarter Typescript

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export class App extends React.Component {
+  public render() {
+    return (
+      <div className="app">
+        <header>
+          <h1>Welcome to React</h1>
+        </header>
+      </div>
+    );
+  }
+}
---
+++ src/pages/sign-up/sign-up.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export class SignUp extends React.Component<any, any> {
+  public render() {
+    return (
+      false
+    )
+  }
+}
